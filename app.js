192e63db47a9c88e2b28c0ea00a4ca4efa5319c5
VINICIUS HONORIO YAZBEK <202301095701@alunos.ibmec.edu.br>
2024-04-26 11:36:54 -0300

    sincronizando no node js arquivo pagina_principal e aula

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/", function (req, res) {
   res.render("pagina_principal")
 });
 
+app.get("/aula", function (req, res) {
+  res.render("aula");
+});
+
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
